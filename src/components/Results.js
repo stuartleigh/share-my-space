9579a7cd0a9e298c1b3d3378eb2701aa5e17f693
= <aaronkazah@gmail.com>
2016-10-20 16:58:12 +0100

    improved results

--- src/components/Results.js
+++ src/components/Results.js
@@ -4,6 +4,9 @@ import Result from './Result';
 
 export default class Results extends Component {
   render () {
+
+
+
     return (
       <div className='container'>
         <h3 className='results-text'>{'Recommended:'}</h3>
