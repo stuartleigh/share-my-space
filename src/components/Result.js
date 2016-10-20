1ba3bce340e623a64a0c0ac1f94e0673d4f9f337
= <aaronkazah@gmail.com>
2016-10-20 17:24:33 +0100

    added the corrections

--- src/components/Result.js
+++ src/components/Result.js
@@ -28,7 +28,7 @@ export default class Result extends Component {
             <div className='result-paragraph'>{"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem"}</div>
             <div>
                 <div className='result-amount'>£{value}</div>
-                <div className='result-explainer'>estimated revenue</div>
+                <div className='result-explainer'>estimated monthly revenue</div>
             </div>
             <a href={links[type]}><button className='result-button' type='button'>Show Me How</button></a>
         </div>
--- src/components/Results.js
+++ src/components/Results.js
@@ -11,7 +11,7 @@ export default class Results extends Component {
       <div className='container'>
         <h3 className='results-text'>{'Recommended:'}</h3>
         <Result type="airbb" value={4564} />
-        <div className='horizontal-separator' />
+        <div className='horizontal-separator'></div>
         <h3 className='results-text'>{'Alternatives:'}</h3>
         <Result type="coworking" value={2332} />
         <Result type="appearHere" value={1222} />
